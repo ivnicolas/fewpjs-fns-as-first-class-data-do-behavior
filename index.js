7f24294819a7671552ec7374ac4b5f5f119dc2bc
ivnicolas <ikennanicolas@gmail.com>
2021-05-27 17:59:17 -0400

    commit tree 145af8b962f4d5f29c17bf1a7efbe8702820c573

--- index.js
+++ index.js
@@ -10,4 +10,21 @@ function handleClick(e) {
 /* End Given Code, don't edit above here...*/
 
 /* Write your implementation of greet() */
+
+function greet(time){
+  let convTime = parseInt(time)
+  if(convTime < 12){
+    return "Good Morning"
+  }
+  else if (convTime > 12 && convTime < 17){
+    return "Good Afternoon"
+  }
+  else {
+    return "Good Evening"
+  }
+}
 /* Write your implementation of displayMessage() */
+
+function displayMessage(message){
+  document.getElementById("greeting").innerText = message
+}
